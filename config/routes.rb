b52dfc1d1810e3b4d4a587c87a678484aaba78c9
DavidNNussbaum <brocknynj@aol.com>
2021-10-26 12:29:21 -0400

    routes.rb is modified and .env is created and populated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+  resources :conversations, except: [:index]
+  resources :patients do 
+    resources :conversations, only: [:index]
+  end
+  get "/current_user", to: "current_user#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
+ 
